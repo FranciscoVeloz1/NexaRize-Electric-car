a9e184bf79077b4020d3e629494c61be7d9a7780
FranciscoVeloz <franciscoveloz245@gmail.com>
2023-08-20 11:59:01 -0600

    feat: the motor works as should does

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { CarEntity } from "./entities/state.entity";
 class ElectricCar {
   state: CarEntity;
   board: five.Board;
-  onButton: five.Button;
+  turnButton: five.Button;
   forwardButton: five.Button;
   parkingButton: five.Button;
   reverseButton: five.Button;
@@ -15,49 +15,45 @@ class ElectricCar {
   pedal: five.Sensor;
   motorIn1: five.Led;
   motorIn2: five.Led;
+  motorEn: five.Led;
 
   constructor() {
-    this.board = new five.Board({ port: "/dev/ttyACM0" });
+    // this.board = new five.Board({ port: "/dev/ttyACM0" });
+    this.board = new five.Board({ port: "/dev/ttyUSB0" });
     this.state = {
       carState: {
         isOn: false,
         mode: "parking",
       },
     };
-    this.InitializeBoard();
 
-    this.TurnOnCarHandler = this.TurnOnCarHandler.bind(this);
-    this.TurnOffCarHandler = this.TurnOffCarHandler.bind(this);
-    this.ModeParkingHandler = this.ModeParkingHandler.bind(this);
-    this.ModeForwardHandler = this.ModeForwardHandler.bind(this);
-    this.ModeReverseHandler = this.ModeReverseHandler.bind(this);
-    this.PedalHandler = this.PedalHandler.bind(this);
-    this.MotorHandler = this.MotorHandler.bind(this);
+    this.InitializeBoard();
   }
 
   InitializeBoard() {
     this.board.on("ready", () => {
       // Buttons
-      this.onButton = new five.Button(4);
-      this.forwardButton = new five.Button(6);
+      this.turnButton = new five.Button(4);
       this.parkingButton = new five.Button(5);
+      this.forwardButton = new five.Button(6);
       this.reverseButton = new five.Button(7);
 
       // Leds
       this.turnLed = new five.Led(8);
-      this.forwardLed = new five.Led(10);
       this.parkingLed = new five.Led(9);
-      this.reverseLed = new five.Led(12);
+      this.forwardLed = new five.Led(10);
+      this.reverseLed = new five.Led(11);
 
       // Motors
-      this.motorIn1 = new five.Led(3);
-      this.motorIn2 = new five.Led(11);
+      this.motorIn1 = new five.Led(2);
+      this.motorIn2 = new five.Led(3);
+      this.motorEn = new five.Led(12);
 
       // Potentiometer
       this.pedal = new five.Sensor("A5");
 
       // Events
-      this.onButton.on("press", this.TurnOnCarHandler);
+      this.turnButton.on("press", this.TurnOnCarHandler);
       this.parkingButton.on("press", this.ModeParkingHandler);
       this.forwardButton.on("press", this.ModeForwardHandler);
       this.reverseButton.on("press", this.ModeReverseHandler);
@@ -66,8 +62,8 @@ class ElectricCar {
   }
 
   // Turn On/Off the car
-  TurnOnCarHandler() {
-    if (this.pedal.scaleTo(0, 255) > 30) return;
+  TurnOnCarHandler = () => {
+    // if (this.pedal.scaleTo(0, 255) > 30) return;
     if (this.state.carState.isOn) return this.TurnOffCarHandler();
 
     this.turnLed.on();
@@ -75,9 +71,9 @@ class ElectricCar {
     this.state.carState.isOn = true;
     console.log("Turning on the car...");
     return;
-  }
+  };
 
-  TurnOffCarHandler() {
+  TurnOffCarHandler = () => {
     if (this.state.carState.mode !== "parking") return;
     this.turnLed.off();
     this.forwardLed.off();
@@ -85,10 +81,10 @@ class ElectricCar {
     this.reverseLed.off();
     this.state.carState.isOn = false;
     console.log("Turning off the car...");
-  }
+  };
 
   // Car modes
-  ModeParkingHandler() {
+  ModeParkingHandler = () => {
     if (!this.state.carState.isOn) return;
     if (this.pedal.scaleTo(0, 255) > 40) return;
     if (this.state.carState.mode === "parking") return;
@@ -97,9 +93,9 @@ class ElectricCar {
     this.forwardLed.off();
     this.reverseLed.off();
     this.state.carState.mode = "parking";
-  }
+  };
 
-  ModeForwardHandler() {
+  ModeForwardHandler = () => {
     if (!this.state.carState.isOn) return;
     if (this.pedal.scaleTo(0, 255) > 40) return;
     if (this.state.carState.mode === "forward") return;
@@ -108,9 +104,9 @@ class ElectricCar {
     this.parkingLed.off();
     this.reverseLed.off();
     this.state.carState.mode = "forward";
-  }
+  };
 
-  ModeReverseHandler() {
+  ModeReverseHandler = () => {
     if (!this.state.carState.isOn) return;
     if (this.pedal.scaleTo(0, 255) > 40) return;
     if (this.state.carState.mode === "reverse") return;
@@ -119,43 +115,55 @@ class ElectricCar {
     this.parkingLed.off();
     this.forwardLed.off();
     this.state.carState.mode = "reverse";
-  }
+  };
 
   // Car speed
-  PedalHandler() {
+  PedalHandler = () => {
     try {
       if (!this.state.carState.isOn) return;
       const speed = this.pedal.scaleTo(0, 255);
-      console.log(`Motor Speed: ${speed}`);
-      this.MotorHandler(speed);
+      this.MotorSpeedHandler(speed);
     } catch (error) {
       if (error instanceof Error) console.log(error.message);
     }
-  }
+  };
 
-  MotorHandler(speed: number) {
+  MotorHandler = (speed: number, direction: "forward" | "reverse" | "stop") => {
+    if (direction === "forward") {
+      console.log(speed);
+      this.motorIn1.on();
+      this.motorIn2.off();
+      this.motorEn.brightness(speed);
+    } else if (direction === "reverse") {
+      console.log(speed);
+      this.motorIn1.off();
+      this.motorIn2.on();
+      this.motorEn.brightness(speed);
+    } else {
+      this.motorIn1.off();
+      this.motorIn2.off();
+      this.motorEn.brightness(0);
+    }
+  };
+
+  MotorSpeedHandler = (speed: number) => {
     if (speed < 20) {
-      this.motorIn1.brightness(0);
-      this.motorIn2.brightness(0);
-      return;
+      this.MotorHandler(0, "stop");
     }
 
     const { mode } = this.state.carState;
     if (mode === "parking") {
-      this.motorIn1.brightness(0);
-      this.motorIn2.brightness(0);
+      this.MotorHandler(0, "stop");
     }
 
     if (mode === "forward") {
-      this.motorIn1.brightness(speed);
-      this.motorIn2.brightness(0);
+      this.MotorHandler(speed, "forward");
     }
 
     if (mode === "reverse") {
-      this.motorIn1.brightness(0);
-      this.motorIn2.brightness(speed);
+      this.MotorHandler(speed, "reverse");
     }
-  }
+  };
 }
 
 new ElectricCar();
