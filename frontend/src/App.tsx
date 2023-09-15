2dc10d8ad3db9cc85bbb8577b8b94674e26d7286
FranciscoVeloz <franciscoveloz245@gmail.com>
2023-09-14 19:18:02 -0600

    feat: added the Car board

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,42 @@
-import Tachometer from "@components/Tachometer";
+import CarBoard from "@components/CarBoard";
+import { ChangeEvent, useState } from "react";
 
 const App = () => {
+  const [sliderValue, setSliderValue] = useState(50);
+  const [mode, setMode] = useState<"P" | "R" | "D" | "N">("P");
+
+  const handleSliderChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSliderValue(Number(event.target.value));
+  };
+
+  const handleMode = () => {
+    const modes = ["P", "N", "D", "R"];
+    const currentIndex = modes.indexOf(mode);
+
+    if (currentIndex !== -1 && currentIndex < modes.length - 1) {
+      setMode(modes[currentIndex + 1] as "P" | "R" | "D" | "N");
+    } else {
+      setMode("P");
+    }
+  };
   return (
-    <div>
-      <Tachometer />
+    <div className="w-full h-screen bg-gradient-to-r from-blue-500 to-green-500 p-4">
+      <CarBoard mode={mode} speed={sliderValue} />
+
+      {/* Simulation of the controls */}
+      <div className="flex">
+        <button onClick={handleMode}>Change mode</button>
+
+        <input
+          type="range"
+          min="0"
+          max="280"
+          step="1"
+          value={sliderValue}
+          onChange={handleSliderChange}
+          className="w-full"
+        />
+      </div>
     </div>
   );
 };
